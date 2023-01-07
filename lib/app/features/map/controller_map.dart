907cc1fb342f03fe50bef4920d7682874f196a83
jonathanh-c <67289817+jonathanh-c@users.noreply.github.com>
2023-01-07 11:00:28 -0700

    begining of controller map

--- lib/app/features/map/controller_map.dart
+++ lib/app/features/map/controller_map.dart
@@ -1,1 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maps_flutter_appwrite_test/app/features/map/repository_map.dart';
+
+class ControllerMap extends StateNotifier<AsyncValue<void>> {
+  ControllerMap({required this.repositoryMap}) : super(const AsyncData(null));
+  final RepositoryMap repositoryMap;
+
+
+  Stream<
+}
